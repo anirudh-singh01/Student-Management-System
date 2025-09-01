35aaa58d8beb9739b87d28b78059bdd4459f8a3a
anirudh-singh01 <anirudhsingh.ani321@gmail.com>
2025-09-02 00:00:45 +0530

    Clear all files from repository

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting Student Management System..."
-echo ""
-echo "Make sure you have Java 8+ and Maven installed."
-echo ""
-echo "Starting the application..."
-mvn spring-boot:run
