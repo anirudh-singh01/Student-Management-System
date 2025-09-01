a523fbcad98dd293f2596652d5c54576ac4b4825
Anirudh Singh <129600428+anirudh-singh01@users.noreply.github.com>
2025-09-02 01:33:32 +0530

    Delete run.bat

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Student Management System...
-echo.
-echo Make sure you have Java 8+ and Maven installed.
-echo.
-echo Starting the application...
-mvn spring-boot:run
-pause
